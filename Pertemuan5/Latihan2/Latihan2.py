a50ded13c2fbd640c6cb969adbfe03f7ce7aa3ea
Khikma Mei Rida <44047322+meirida@users.noreply.github.com>
2019-05-14 20:47:23 +0700

    Update Latihan2.py

--- Pertemuan5/Latihan2/Latihan2.py
+++ Pertemuan5/Latihan2/Latihan2.py
@@ -4,30 +4,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-class DemoIcon(QWidget):
- def __init__(self):
-     super().__init__()
-     self.setupUi()
-
- def setupUi(self):
-     self.resize(400, 100)
-     self.move(300, 300)
-     self.setWindowTitle('Tugas Menyisipkan icon')
-
-     self.label = QLabel()
-     self.label.setText('Bahasa Pemrograman Favorit Anda')
-
-     icon1 = QIcon('bahasa/python.png')
-     self.button1 = QPushButton('\tPython')
-     self.button1.setIcon(icon1)
-
-     icon2 = QIcon('bahasa/php.png')
-     self.button2 = QPushButton('\tPHP')import sys
-
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-
 
 class IconButon(QWidget):
     def __init__(self):
@@ -77,32 +53,3 @@ if __name__ == '__main__':
     form = IconButon()
     form.show()
     a.exec_()
-
-     self.button2.setIcon(icon2)
-
-     icon3 = QIcon('bahasa/java.png')
-     self.button3 = QPushButton('\tJava')
-     self.button3.setIcon(icon3)
-
-     icon4 = QIcon('/c-plus-plus.png')
-     self.button4 = QPushButton('\tC/C++ ')
-     self.button4.setIcon(icon4)
-
-     hbox = QHBoxLayout()
-     hbox.addWidget(self.button1)
-     hbox.addWidget(self.button2)
-     hbox.addWidget(self.button3)
-     hbox.addWidget(self.button4)
-
-     layout = QVBoxLayout()
-     layout.addWidget(self.label)
-     layout.addLayout(hbox)
-     self.setLayout(layout)
-
-if __name__ == '__main__':
- a = QApplication(sys.argv)
-
- form = DemoIcon()
- form.show()
-
- a.exec_()
